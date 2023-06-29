b407be7241e6505b4815d9c1fe7c14aebb2c76bf
Гоша Ефремов <efremov.gosh@gmail.com>
2023-06-29 13:05:44 +0300

    Creating_Database

---
+++ BooksWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BooksWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
